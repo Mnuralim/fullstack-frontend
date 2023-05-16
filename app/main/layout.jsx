5f761e87281732f734ff25c55af79956c2d3c2d3
mnuralim <shadownur345.com>
2023-05-17 07:53:45 +0800

    update

--- app/main/layout.jsx
+++ app/main/layout.jsx
@@ -1,14 +1,17 @@
 "use client";
 import Footer from "./footer";
+import { SessionProvider } from "next-auth/react";
 import Header from "./header";
 
 export default function RootLayout({ children }) {
   return (
     <>
-      <Header />
-      {children}
-      <div className="mb-52"></div>
-      <Footer />
+      <SessionProvider>
+        <Header />
+        {children}
+        <div className="mb-52"></div>
+        <Footer />
+      </SessionProvider>
     </>
   );
 }
